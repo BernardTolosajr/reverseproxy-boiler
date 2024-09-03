3558fc8fd4221929264a8508b2547df10ffd03c6
btx <bernardo.tolosa@gcash.com>
2024-09-03 10:01:26 +0800

    added tranparent mode

--- cmd/shield.go
+++ cmd/shield.go
@@ -50,8 +50,10 @@ var shieldcmd = &cobra.Command{
 			return err
 		}
 
-		director := shield.NewDirector(origin)
-		response := shield.NewResponse()
+		tm, err := cmd.Flags().GetBool("transparent-mode")
+		if err != nil {
+			return err
+		}
 
 		exporter, err := prometheus.New()
 		if err != nil {
@@ -59,11 +61,7 @@ var shieldcmd = &cobra.Command{
 		}
 
 		mux := http.NewServeMux()
-		proxy := &httputil.ReverseProxy{
-			Director:       director.Request(),
-			ModifyResponse: response.Modify(),
-			ErrorHandler:   ErrorHandler(),
-		}
+		proxy := createProxy(origin, tm)
 
 		mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 			proxy.ServeHTTP(w, r)
@@ -71,12 +69,21 @@ var shieldcmd = &cobra.Command{
 
 		mux.Handle("/metrics", promhttp.Handler())
 
+		var handler http.Handler
+
 		mw := shield.NewCacheMiddleware(exporter)
+
+		// transparent mode
+		if !tm {
+			handler = mw.Next(mux)
+		} else {
+			handler = mux
+		}
 
 		// TODO: support multiple middleware
 		s := &http.Server{
 			Addr:           ":9000",
-			Handler:        mw.Next(mux),
+			Handler:        handler,
 			ReadTimeout:    10 * time.Second,
 			WriteTimeout:   10 * time.Second,
 			MaxHeaderBytes: 1 << 20,
@@ -86,9 +93,28 @@ var shieldcmd = &cobra.Command{
 	},
 }
 
+func createProxy(origin string, transparentMode bool) *httputil.ReverseProxy {
+	director := shield.NewDirector(origin)
+
+	if !transparentMode {
+		response := shield.NewResponse()
+		return &httputil.ReverseProxy{
+			Director:       director.Request(),
+			ModifyResponse: response.Modify(),
+			ErrorHandler:   ErrorHandler(),
+		}
+	}
+
+	return &httputil.ReverseProxy{
+		Director:     director.Request(),
+		ErrorHandler: ErrorHandler(),
+	}
+}
+
 func init() {
 	cobra.OnInitialize(initConfig)
 	shieldcmd.Flags().Bool("debug-mode", false, "debug mode")
 	shieldcmd.Flags().String("origin-host", "http://localhost:3030", "origin host")
+	shieldcmd.Flags().Bool("transparent-mode", false, "transparent mode")
 	rootCmd.AddCommand(shieldcmd)
 }
